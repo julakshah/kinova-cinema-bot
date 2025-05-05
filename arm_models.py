e8a52858381f707944d9a0c779af28b4d9b4fc27
julakshah <julakshah@gmail.com>
2025-05-05 15:55:31 -0400

    changed calc_inverse_kinematics base code

--- arm_models.py
+++ arm_models.py
@@ -385,174 +385,94 @@ class SixDOFRobot:
             EE: EndEffector object containing desired position and orientation.
             soln: Optional parameter for multiple solutions (not implemented).
         """
+        # Extract position and orientation of the end-effector
+        l1, l2, l3, l4, l5 = self.l1, self.l2, self.l3, self.l4, self.l5
 
-        # initializing a list for each theta
-        theta1_list = []
-        theta2_list = []
-        theta3_list = []
-        theta4_list = []
-        theta5_list = []
+        # Desired position and rotation matrix
+        p = np.array([EE.x, EE.y, EE.z])
+        rpy = np.array([EE.rotx, EE.roty, EE.rotz])
+        R = euler_to_rotm(rpy)
 
-        # calculating theta 1 value and then adding it to the list
-        theta_1 = np.arctan2(EE.y, EE.x)
-        theta1_list.append(theta_1)
+        # Calculate wrist position
+        wrist_pos = p - R @ np.array([0, 0, 1]) * (l4 + l5)
+        # print(f"Wrist pose: \n {wrist_pos} \n")
+        # print(f"Rotation matrix, R: {R}, \n Determinant: {np.linalg.det(R)}")
 
-        # calculating and adding the other theta 1 value
-        if theta_1 > 0:
-            theta1_list.append(theta_1 - np.pi)  # angle wrapping case 1
-        else:
-            theta1_list.append(theta_1 + np.pi)  # angle wrapping case 2
+        try:
+            # Solve for theta_1 using trigonometry
+            x_wrist, y_wrist, z_wrist = wrist_pos
+            theta1 = [atan2(y_wrist, x_wrist), wraptopi(atan2(y_wrist, x_wrist) + PI)]
+            # TIP #1: the wraptopi ensures that the values stays within -pi to pi
 
-        # find rotation of EE matrix
-        EE_euler = (EE.rotx, EE.roty, EE.rotz)
-        EE_rot = np.array(euler_to_rotm(EE_euler))
-
-        # find position of joint 3 in base frame
-        P_EE = np.array([EE.x, EE.y, EE.z])
-        k = np.array([0, 0, 1])
-
-        # subtracting l4 and l5 in the z direction from the end effector pos
-        # P_3 = P_EE - ((self.l4 + self.l5) * (EE_rot @ k))
-        P_3 = P_EE - ((self.l7) * (EE_rot @ k))
-
-        # Solve decoupled kinematic problem in frame 1 for theta 2 & 3
-        P3_x, P3_y, P3_z = P_3[0], P_3[1], P_3[2]
-        L = np.sqrt(P3_x**2 + P3_y**2 + (P3_z - self.l1) ** 2)  # solve for L in 3D
-
-        # Check if point is reachable
-        max_reach = self.l2 + self.l3  # maximum reach
-        min_reach = abs(self.l2 - self.l3)  # minimum reach
-
-        if L > max_reach or L < min_reach:
-            print(
-                f"Point unreachable: Distance {L:.3f} is outside arm's reach [{min_reach:.3f}, {max_reach:.3f}]"
-            )
-
-        delta_x = np.sqrt(P3_x**2 + P3_y**2)  # solve for x displacement from 1 to 3
-        delta_z = P3_z - self.l1  # solve for z displacement from 1 to 3
-        alpha = np.arctan2(delta_z, delta_x)
-
-        # calculate phi using law of cosine
-        phi = np.arccos((self.l2**2 + self.l3**2 - L**2) / (2 * self.l2 * self.l3))
-        # adding positive and negative variations of theta 3 to a list
-        theta3_list.append(np.pi - phi)
-        theta3_list.append(-(np.pi - phi))
-
-        # calculating theta 2
-        # first solution of theta 2
-        gamma1 = self.l3 * sin(theta3_list[0])
-        beta1 = np.arcsin(gamma1 / L)
-        theta2_list.append(-(alpha - beta1 - np.pi / 2))
-
-        # second solution of theta 2
-        gamma2 = self.l3 * sin(theta3_list[1])
-        beta2 = np.arcsin(gamma2 / L)
-        theta2_list.append(alpha + beta2 - np.pi / 2)
-
-        # We compute R_0_3 by plugging in thetas 1 2 and 3 into the rotation matrix
-        # multiply wrist pos: R_3_5 = (R_0_3)^T * R_0_6 (transpose is the same thing as inverse in this case)
-        # R_0_5 is the end effector orientation (found from the eulertorotm function of the end effector)
-        # Calculate R_3_5, gives a 3x3 matrix of values. These values mean nothing unless you know the functions
-        # they are associated with. We can matrix multiply symbolically R_3_4 and R_4_5 which
-        # will symbolically make R_3_5. With these, there are a few cos and sin functions that standalone with a theta
-        # (ex. r_3_5[1,2] = sin(theta4)). With some simple inverse trigonometric functions, we can find what the theta values are.
-
-        # looping through all combinations of theta 1 2 to get all combinations of theta 4 and 5
-        for i in range(2):
-            for j in range(2):
-                theta_1 = theta1_list[i]
-                theta_2 = theta2_list[j]
-                theta_3 = theta3_list[j]
-                # DH matrix for current theta values
-                dh1 = dh_to_matrix([theta_1, self.l1, 0, np.pi / 2])
-                dh2 = dh_to_matrix([theta_2 + np.pi / 2, 0, self.l2, 0])
-                dh3 = dh_to_matrix([-theta_3, 0, self.l3, 0])
-
-                # getting the rotation matrix of 0 to 3 from dh matrix
-                t_0_3 = dh1 @ dh2 @ dh3
-                r_0_3 = t_0_3[:3, :3]
-                # getting the rotation matrix from 3 to 5
-                r_3_5 = np.transpose(r_0_3) @ EE_rot
-
-                # EXPLAIN THIS IN WORDS
-                theta_4 = wraptopi(np.arctan2(r_3_5[1, 2], r_3_5[0, 2]))
-
-                theta_5 = wraptopi(np.arctan2(r_3_5[2, 0], r_3_5[2, 1]))
-
-                # adding theta 4 and 5 to their list
-                theta4_list.append(theta_4)
-                theta5_list.append(theta_5)
-
-        # adding all combinations of theta values into the solutions list
-        solutions = []
-        solutions.append(
-            [
-                theta1_list[1],
-                theta2_list[0],
-                theta3_list[0],
-                theta4_list[2],
-                theta5_list[1],
+            # using cosine rule, find theta_3
+            s = z_wrist - l1
+            r = [-sqrt(x_wrist**2 + y_wrist**2), sqrt(x_wrist**2 + y_wrist**2)]
+            # TIP #2: consider two cases for r (+ve and -ve)
+            L = sqrt(r[0] ** 2 + s**2)
+            ctheta3 = (L**2 - l2**2 - l3**2) / (2 * l2 * l3)
+            theta3 = [
+                atan2(sqrt(1 - ctheta3**2), ctheta3),
+                atan2(-sqrt(1 - ctheta3**2), ctheta3),
             ]
-        )
-        solutions.append(
-            [
-                theta1_list[1],
-                theta2_list[1],
-                theta3_list[1],
-                theta4_list[2],
-                theta5_list[1],
-            ]
-        )
-        solutions.append(
-            [
-                theta1_list[0],
-                theta2_list[0],
-                theta3_list[0],
-                theta4_list[1],
-                theta5_list[2],
-            ]
-        )
-        solutions.append(
-            [
-                theta1_list[0],
-                theta2_list[1],
-                theta3_list[1],
-                theta4_list[1],
-                theta5_list[2],
-            ]
-        )
+            # TIP #3: Alternative approach to finding theta3
+            # beta = np.arccos((-L**2 + l2**2 + l3**2) / (2 * l2 * l3))
+            # theta3 = [pi - beta, beta - pi]
 
-        # initializing error list
-        error_list = []
-        # looping through each solution
-        for i, solution in enumerate(solutions):
-            # calculationg the position of this solution
-            position = [EE.x, EE.y, EE.z, EE.rotx, EE.roty, EE.rotz]
-            self.calc_forward_kinematics(solution, radians=True)
-            calc_pos = [
-                self.ee.x,
-                self.ee.y,
-                self.ee.z,
-                self.ee.rotx,
-                self.ee.roty,
-                self.ee.rotz,
-            ]
-            # calculating the error from desired and actual position and adding it to the list
-            error_list.append(
-                [np.linalg.norm(np.array(position) - np.array(calc_pos)), i]
-            )
-        # sorting the error
-        sorted_indices = np.argsort(np.array(error_list)[:, 0])
+            possible_solns, valid_solns = [], []
 
-        # displaying the 4 positions in sorted order for each button
-        if soln == 0:
-            self.calc_forward_kinematics(solutions[sorted_indices[0]], radians=True)
-        elif soln == 1:
-            self.calc_forward_kinematics(solutions[sorted_indices[1]], radians=True)
-        elif soln == 2:
-            self.calc_forward_kinematics(solutions[sorted_indices[2]], radians=True)
-        elif soln == 3:
-            self.calc_forward_kinematics(solutions[sorted_indices[3]], radians=True)
+            for th3 in theta3:
+                # calculate theta2
+                theta2 = [
+                    atan2(r[0], s) - atan2(l3 * sin(-th3), l2 + l3 * cos(-th3)),
+                    atan2(r[1], s) - atan2(l3 * sin(-th3), l2 + l3 * cos(-th3)),
+                ]
+                # TIP #4: We set theta3 to -ve because it's moving in the opposite direction to theta2
+
+                # calculate theta4 and theta5
+                for th2 in theta2:
+                    c23_ = cos(th2) * cos(th3) + sin(th2) * sin(th3)
+                    s23_ = sin(th2) * cos(th3) - cos(th2) * sin(th3)
+
+                    for th1 in theta1:
+                        c1, s1 = cos(th1), sin(th1)
+                        R0_3 = np.array(
+                            [
+                                [-c1 * s23_, -c1 * c23_, s1],
+                                [-s1 * s23_, -s1 * c23_, -c1],
+                                [c23_, -s23_, 0],
+                            ]
+                        )
+                        # TIP #5 This is derived from calculating R3_4*R4_5 from the DH table
+                        R3_5 = R0_3.T @ R
+
+                        th4 = atan2(R3_5[1, 2], R3_5[0, 2])
+                        th5 = atan2(-R3_5[2, 0], -R3_5[2, 1])
+
+                        solution = [th1, th2, th3, th4, th5]
+                        possible_solns.append(solution)
+                        if self.check_valid_ik_soln(solution, EE):
+                            valid_solns.append(solution)
+
+            if len(valid_solns) > 0:
+                if soln == 0:
+                    self.theta = valid_solns[0]
+                elif soln == 1:
+                    self.theta = valid_solns[1]
+
+                # Calculate forward kinematics with the new joint angles
+                self.calc_forward_kinematics(self.theta, radians=True)
+
+                return True
+
+            else:
+                print(
+                    f"\n[Analytical IK solver] ERROR: No valid solution found! Joint limits may be exceeded or position may be unreachable."
+                )
+                # raise ValueError
+                return False
+
+        except RuntimeWarning:
+            print("\n [ERROR] (Runtime) Joint limits exceeded! \n")
 
     def calc_numerical_ik(self, EE: EndEffector, tol=0.01, ilimit=100):
         """
